34a33974c13f5ae7ce2368af9e4411ae16acef03
gayatrirajbanshi <rajbanshigayatri31@gmail.com>
2026-05-26 17:47:49 +0545

    changes

--- backend/internship_portal/internship_portal/urls.py
+++ backend/internship_portal/internship_portal/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/auth/', include('core.urls')),
+    path('api/students/', include('students.urls')),
+    path('api/companies/', include('companies.urls')),
+    path('api/applications/', include('applications.urls')),
+    path('api/recommendations/', include('recommendations.urls')),
+    path('api/admin-panel/', include('admin_panel.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
