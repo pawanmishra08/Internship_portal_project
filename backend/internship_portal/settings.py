f810f0d6f760213f609467affe9223405561bfd1
pawan mishra <pawanm9816@gmail.com>
2026-05-31 18:00:41 +0545

    deploy changes

--- backend/internship_portal/settings.py
+++ backend/internship_portal/settings.py
@@ -36,8 +36,7 @@ SECRET_KEY = 'django-insecure-utiod(axlhm^*zeuw1!_-$5+_rgwh!hotn_wqnq5m*)qhr9_=4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Auth
 AUTH_USER_MODEL = 'core.User'
